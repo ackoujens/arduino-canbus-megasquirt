42b3f2844a9a1543fac7d15aeef9fbc292193b9a
Ackou Jens <ackoujens@gmail.com>
2021-11-17 13:21:23 +0100

    Basic setup and board test

---
+++ arduino-megasquirt-canbus.ino
@@ -0,0 +1,22 @@
+/*
+  MegaSquirt CAN bus
+
+  Sends and receives data from and to MegaSquirt.
+
+  A lot of data can be sent over a CAN network and makes it easier on wiring.
+  Everything can be connected to this CAN node and act as an extention to the
+  current MegaSquirt expandability.
+
+  by Jens Ackou
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  delay(5000);
+  Serial.println("LOOP DONE");
+}
+
